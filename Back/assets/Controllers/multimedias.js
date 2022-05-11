ed1c98c6b7a17525e1867368172e2029b676ee8d
Neveta0905 <lulued@free.fr>
2022-05-11 21:43:56 +0200

    Multimedia controller skeleton and auth

---
+++ Back/assets/Controllers/multimedias.js
@@ -0,0 +1,23 @@
+exports.getOne = (req,res) => {
+
+}
+
+exports.getAll = (req,res) => {
+
+}
+
+exports.sendOne = (req,res) => {
+	
+}
+
+exports.deleteOne = (req,res) => {
+	
+}
+
+exports.deleteMany = (req,res) => {
+	
+}
+
+exports.modifyOne = (req,res) => {
+	
+}
